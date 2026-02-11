63a27105da796a242fa04282df07e975cab48a71
RemoteRoster <RemoteRoster@hotmail.com>
2026-02-12 00:37:15 +0300

    Create help command file

---
+++ commands/help.js
@@ -0,0 +1,8 @@
+// Help command implementation
+module.exports = {
+    name: 'help',
+    description: 'List all available commands',
+    execute: (args) => {
+        // Display help information
+    },
+};
