4986664877ace46c1b0e16c5746265f81b8cdf21
RemoteRoster <RemoteRoster@hotmail.com>
2026-02-12 00:45:27 +0300

    Added global error handling for unhandled promise rejections and unhandled exceptions.

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
-// Improved index.js for RemoteRosterBot
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf('YOUR_BOT_TOKEN');
+// Global error handling
 
-// Helper function to create inline keyboard buttons
-const createInlineKeyboard = (options) => {
-    return options.map(option => [{ text: option.text, callback_data: option.callback_data }]);
-};
-
-// Start command handler
-bot.start((ctx) => {
-    ctx.reply('Welcome to RemoteRosterBot! Please choose an option:', {
-        reply_markup: {
-            inline_keyboard: createInlineKeyboard([
-                { text: 'Option 1', callback_data: 'option1' },
-                { text: 'Option 2', callback_data: 'option2' },
-                { text: 'Help', callback_data: 'help' }
-            ])
-        }
-    });
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-// Callback query handler
-bot.on('callback_query', (ctx) => {
-    const callbackData = ctx.callbackQuery.data;
-    switch (callbackData) {
-        case 'option1':
-            ctx.answerCbQuery('You selected Option 1!');
-            // Implement additional logic for Option 1
-            break;
-        case 'option2':
-            ctx.answerCbQuery('You selected Option 2!');
-            // Implement additional logic for Option 2
-            break;
-        case 'help':
-            ctx.answerCbQuery('Help information will be provided here.');
-            // Provide help information
-            break;
-        default:
-            ctx.answerCbQuery('Unknown option. Please try again.');
-            break;
-    }
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception thrown:', error);
 });
 
-// Error handling
-bot.catch((err, ctx) => {
-    console.error(`Error encountered for ${ctx.updateType}:`, err);
-    ctx.reply('An error occurred, please try again later.');
-});
-
-// Start the bot
-bot.launch();
-
-console.log('Bot is running...');
+// Remaining index.js logic...
